6878862e42330836a6350d709f9462a8defc48d5
Carsen Decker <cad631@nyu.edu>
2019-11-18 16:25:47 -0500

    redoing hallway rip, commented code

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Carsen Decker
+//USAGE: Put this on the player object in order to control its movements
 public class PlayerController : MonoBehaviour
 {
     public float MoveSpeed;
     public float Gravity;
     
-
-    private Rigidbody rb;
+//    private Rigidbody rb;
     private CharacterController cc;
     private Vector3 tempVel;
     
@@ -19,6 +20,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //Takes the input axis and adds it to a temporary velocity vector
         tempVel = Input.GetAxis("Horizontal") * transform.right;
         tempVel += Input.GetAxis("Vertical") * transform.forward;
     }
@@ -26,7 +28,10 @@ public class PlayerController : MonoBehaviour
     //Sets the player velocity based on the input values
     private void FixedUpdate()
     {
+        //Uses the character controller to move based on the axis inputs and set movement speed, and the set gravity as well
         cc.Move(new Vector3(tempVel.x * MoveSpeed, -Gravity, tempVel.z * MoveSpeed) * Time.deltaTime);
+        
+        
 //        rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(tempVel.x * MoveSpeed, rb.velocity.y, tempVel.z * MoveSpeed), 0.8f);
     }
 }
--- Assets/Scripts/System/AudioSystem.cs
+++ Assets/Scripts/System/AudioSystem.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Carsen Decker
+//USAGE: Put this on an object tagged "AudioSystem" with 2 AudioSources
+//FUNCTION: Acts as a centralized place to handle playing audio. Especially useful for managing narration lines overlapping each other
 public class AudioSystem : MonoBehaviour
 {
     public AudioSource NarrationAudio;
     public AudioSource SoundEffectAudio;
-    
-    public 
 
-    void Update()
-    {
-        
-    }
     
+    /// <summary>
+    /// Originally used to play a list of VoiceLines in sequence, playing next one after the last one is finished
+    /// </summary>
+    /// <param name="voiceLines">An array of VoiceLines, a struct that contains both an audio file and a subtitle string</param>
     IEnumerator PlayNarrationSeries(VoiceLine[] voiceLines)
     {
         foreach (VoiceLine line in voiceLines)
@@ -33,6 +34,10 @@ public class AudioSystem : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Plays a line of narration from its assigned AudioSource. Will cut off the currently playing voice line if one is playing
+    /// </summary>
+    /// <param name="clip">The audio clip to play</param>
     public void PlayNarration(AudioClip clip)
     {
         NarrationAudio.Stop();
